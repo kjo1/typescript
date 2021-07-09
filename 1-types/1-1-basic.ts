56347d62c94fb9bd30718eb74af56ea5eb1d950b
Kanghyun Jo <kjo0@outlook.com>
2021-07-09 06:14:12 -0400

    number, string, boolean, undefined, null

--- 1-types/1-1-basic.ts
+++ 1-types/1-1-basic.ts
@@ -1,15 +1,31 @@
 {
-	// // JavaScript
-	// // old: var 💩
-  // ! Don't use var it's the old way of declaring before ES6
-	// var age = 5;
-	// age = 1;
+	/**
+	 * ^ JavaScript
+	 * Primitive: number, string, boolean, bigint, symbol, null, undefined
+	 * Object: function, array.....
+	 */
 
-	// let es6
-	let name = 'hello';
-	name = 'hi';
+	// ^ TypeSCript
+	const num: number = -6;
 
-	// const
-	const age = 5;
-	age = 4;
+	// string
+	const str: string = 'hello';
+
+	// boolean
+	const bool: boolean = false;
+
+	// undefined
+	let name: undefined; // 💩 Don't ever declare as undefined because we can only declare undefined afterwared
+	let age: number | undefined; // ! use as an optional type
+	age = undefined;
+	age = 1;
+	function find(): number | undefined {
+		return undefined;
+	}
+
+	// * between undefined and null, undefined is used more
+
+	// null
+	let person: null; // 💩 The same goes for null as undefined
+	let person2: string | null;
 }
