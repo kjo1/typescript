0a353738e34308b016ceacc8d7d8e89e8d766622
Kanghyun Jo <kjo0@outlook.com>
2021-07-09 06:02:22 -0400

    A simple note for var vs let

---
+++ 1-types/1-1-basic.ts
@@ -0,0 +1,15 @@
+{
+	// // JavaScript
+	// // old: var 💩
+  // ! Don't use var it's the old way of declaring before ES6
+	// var age = 5;
+	// age = 1;
+
+	// let es6
+	let name = 'hello';
+	name = 'hi';
+
+	// const
+	const age = 5;
+	age = 4;
+}
