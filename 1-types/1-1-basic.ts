1101e47c8fdfc1cb7a9c62ffd2308f408f128992
Kanghyun Jo <kjo0@outlook.com>
2021-07-11 21:09:15 -0400

    unknown, any, void, never, object

--- 1-types/1-1-basic.ts
+++ 1-types/1-1-basic.ts
@@ -5,7 +5,7 @@
 	 * Object: function, array.....
 	 */
 
-	// ^ TypeSCript
+	// ^ TypeScript
 	const num: number = -6;
 
 	// string
@@ -28,4 +28,40 @@
 	// null
 	let person: null; // 💩 The same goes for null as undefined
 	let person2: string | null;
+
+	// unknown 💩 - don't know what type can be decalred
+	// ! avoid using it - unknown type exists because TS has to be used with JS
+	let notSure: unknown = 0; // you can declare any types
+	notSure = 'he';
+	notSure = true;
+
+	// any 💩 - accept any types
+	// ! avoid using it
+	let anything: any = 0;
+	anything = 'hello';
+
+	// void - when a function returns nothing
+	// if a type is not declared, the default is void
+	function print(): void {
+		console.log('hello');
+		return;
+	}
+	let unusable: void = undefined; // 💩 never use this way because only undefined can be returned
+
+	// never
+	// ! usually use when an unexpected error occurs to log error message to server
+	// ! because it doesn't return anything
+	function throwError(message: string): never {
+		// message -> server (log)
+		throw new Error(message);
+		while (true) {}
+	}
+	let neverEnding: never; // 💩
+
+	// object
+	// ! can also use as any time so avoid using
+	let obj: object; //💩
+	function acceptSomeObject(obj: object) {}
+	acceptSomeObject({ name: 'sonsy' });
+	acceptSomeObject({ animal: 'dog' });
 }
