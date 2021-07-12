e523c943b663027072e6c863fd79fc84a19fdee4
Kanghyun Jo <kjo0@outlook.com>
2021-07-11 21:55:28 -0400

    Funtion types (spread, default, optional)

--- 1-types/1-2-function.ts
+++ 1-types/1-2-function.ts
@@ -1,5 +1,5 @@
 {
-	// JavaScript 💩
+	/* 	// JavaScript 💩
 	function jsAdd(num1, num2) {
 		return num1 + num2;
 	}
@@ -26,5 +26,35 @@
 		return new Promise((resolve, reject) => {
 			resolve(100);
 		});
+	} */
+
+	// JavaScript ✨ => TypeScript
+	// optional parameter
+	// 1.
+	function printName(firstName: string, lastName?: string) {
+		// 2. function printName(firstName: string, lastName: string | undefined) {
+		// * don't do #2(the above) way because you have to give undefined value every time is undefined alway do as #1 when it's optional parameter
+		// function printName(firstName: string, lastName: string = 'Unknown') {
+		console.log(firstName);
+		console.log(lastName); // undefined
 	}
+	printName('Steve', 'Jobs');
+	printName('Sonsy');
+	printName('Anna');
+
+	// Default parameter
+	function printMessage(message: string = 'default message') {
+		console.log(message);
+	}
+	printMessage();
+
+	// Rest parameter
+	function addNumbers(...numbers: number[]): number {
+		return numbers.reduce((a, b) => a + b);
+		console.log(numbers);
+	}
+
+	console.log(addNumbers(1, 2));
+	console.log(addNumbers(1, 2, 3, 4));
+	console.log(addNumbers(1, 2, 3, 4, 5, 0));
 }
