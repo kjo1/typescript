2eca3b0568739ecf95241a575d40a049d51f2eac
Kanghyun Jo <kjo0@outlook.com>
2021-07-13 22:11:08 -0400

    Finish loading state

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -1,26 +1,36 @@
 {
-  /**
-   * Print Loading State
-   */
-  type LoadingState = {
-    state: 'loading';
-  };
+	/**
+	 * Print Loading State
+	 */
+	type LoadingState = {
+		state: 'loading';
+	};
 
-  type SuccessState = {
-    state: 'success';
-    response: {
-      body: string;
-    };
-  };
+	type SuccessState = {
+		state: 'success';
+		response: {
+			body: string;
+		};
+	};
 
-  type FailState = {
-    state: 'fail';
-    reason: string;
-  };
+	type FailState = {
+		state: 'fail';
+		reason: string;
+	};
 
-  type ResourceLoadState = LoadingState | SuccessState | FailState;
+	type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+	printLoginState({ state: 'loading' }); // 👀 loading...
+	printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+	printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+	function printLoginState(result: ResourceLoadState) {
+		if ('response' in result) {
+			console.log(`😃 ${result.response.body}`);
+		} else if ('reason' in result) {
+			console.log(`😱 ${result.reason}`);
+		} else {
+			console.log(`👀 loading...`);
+		}
+	}
 }
