70173737d7025ffd7242d59034c0b967d4ba8c34
Kanghyun Jo <kjo0@outlook.com>
2021-07-13 21:01:20 -0400

    Refactor calculator with the tutorial

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -7,6 +7,7 @@ console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
 
+/* tried by myself
 function calculate(operator: string, num1: number, num2: number): number {
 	let result = 0;
 
@@ -23,4 +24,24 @@ function calculate(operator: string, num1: number, num2: number): number {
 	}
 
 	return result;
+}
+ */
+
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+// refactor with the tutorial (used union type)
+function calculate(command: Command, a: number, b: number): number {
+	switch (command) {
+		case 'add':
+			return a + b;
+		case 'substract':
+			return a - b;
+		case 'multiply':
+			return a * b;
+		case 'divide':
+			return a / b;
+		case 'remainder':
+			return a % b;
+		default:
+			throw new Error('unknown command');
+	}
 }
